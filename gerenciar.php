eca01a1aa7dd10bdeb678bf474c54a79cf285706
joaoPedro <Usurio@DESKTOP-7DSI397.(none)>
2023-07-25 21:04:17 -0300

    "updates"

--- gerenciar.php
+++ gerenciar.php
@@ -98,7 +98,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Gerenciar</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="src/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   </head>
   <body class="overflow-hidden">
